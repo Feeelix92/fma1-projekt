8d64576fcda3da2f30c185c191e1c36fcb450747
nicklas <nick1196@web.de>
2021-05-24 13:52:27 +0200

    Initial commit

---
+++ Reiseapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reiseapp
+//
+//  Created by Nicklas Düringer on 24.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Reiseapp/ReiseappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReiseappApp.swift
+//  Reiseapp
+//
+//  Created by Nicklas Düringer on 24.05.21.
+//
+
+import SwiftUI
+
+@main
+struct ReiseappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
